247a5ab433af37d4c349e51d770767c2b80aef8d
basua0017 <basu.arka0017@outlook.com>
2025-05-25 12:53:39 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+
+def get_requirements() -> list[str]:
+    requirements_list = list[str] = []
+
+    
+    return requirements_list
+
+
+
+
+setup(
+    name = "sensor",
+    version= "0.0.1",
+    author= "arka",
+    author_email="basu.arka0017@outlook.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
